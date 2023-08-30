fd30a1a05331aee179165e23f28bc3da9fee2bd6
Andrew Mullen <139258459+andmulLABS01@users.noreply.github.com>
2023-08-30 18:01:43 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 #The purpors of this script is to run a cron job every Friday at 11pm, update the server, list the upgradable packages in a created file that has the current date (e.g. update08.25.23.txt).
 #Andrew Mullen - 8/28/23
 
-#Create the cronjob, set the time you want the script to run, and add the script to be run
+#Create the cronjob, set the time you want the script to run (runs every Friday at 11 pm), and add the script to be run
 #crontab -e
 #* 23 * * 5 sh /home/ubuntu/update.sh
 
