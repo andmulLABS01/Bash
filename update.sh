1f7478421f3b4e825f5e3c0a73bc886fb867b157
Andrew Mullen <139258459+andmulLABS01@users.noreply.github.com>
2023-09-05 12:31:17 -0400

    Update update.sh
    Removed && apt list --upgradable and Echo Y commands from script to show the number of update packages and remove excess code

--- update.sh
+++ update.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#The purpors of this script is to run a cron job every Friday at 11pm, update the server, list the upgradable packages in a created file that has the current date (e.g. update08.25.23.txt).
+#The purpose of this script is to run a cron job every Friday at 11pm, update the server, and list the upgradable packages in a created file that has the current date (e.g. update08.25.23.txt).
 #Andrew Mullen - 8/28/23
 
 #Create the cronjob, set the time you want the script to run (runs every Friday at 11 pm), and add the script to be run
 #crontab -e
-#* 23 * * 5 sh /home/ubuntu/update.sh
+#0 23 * * 5 sh /home/ubuntu/update.sh
 
 
 
-#Creates the current date varable
+#Creates the current date variable
 now=`date +"%m.%d.%Y"`
 
-#Command to pull the current updates, list the upgradable packages and write them in to a text file that included the current date
-sudo apt update && apt list --upgradable &> /home/ubuntu/upgradable${now}.txt
+#Command to pull the current updates, list how many packages can be upgraded that outputs to the screen (ex. x packages can be upgraded.) and 
+#write them into a text file that includes the current date && apt list --upgradable
+sudo apt update &> /home/ubuntu/upgradable${now}.txt
 
-#Command to upgrade the packages
-echo Y | sudo apt upgrade
+#Command to upgrade the packages echo Y | 
+sudo apt upgrade
