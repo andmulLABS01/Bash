85dca12b6438a4996cdf6f5df73020e17a53c42a
Andrew Mullen <139258459+andmulLABS01@users.noreply.github.com>
2023-09-15 09:25:00 -0400

    Update jenkins_setup.sh
    some grammar changes and additional commands

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-#First update system
+#First update and upgrade the system
 sudo apt update
+sudo apt upgrade
+echo 
 
 #enter the wait command to finish the update
 wait
 
-#download java run time environment
+# Download Java runtime environment
 sudo apt install fontconfig openjdk-17-jre
 
 #enter wait command finish the install
@@ -19,16 +21,23 @@ curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \/usr/sh
 #Then add a Jenkins apt repository entry
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \https://pkg.jenkins.io/debian binary/ | sudo tee \/etc/apt/sources.list.d/jenkins.list > /dev/null
 
-#run update command again  HOw to check for Jenkins package? check to see if this file exists (/etc/apt/sources.list.d/jenkins.list content in file)
+#run the update command again  How do I check for the Jenkins package? check to see if this file exists (/etc/apt/sources.list.d/jenkins.list content in file)
 sudo apt update
 
 #enter the wait command to finish the update
 wait
 
-#put in check statement if/else
-
-#install jenkins if found type y
+#install Jenkins
 sudo apt install jenkins
+
+#install python3.10-venv
+sudo apt install python3.10-venv
+
+#install python-pip
+sudo apt install python-pip
+
+#install unzip
+sudo apt install unzip
 
 #enter wait command finish the install
 wait
